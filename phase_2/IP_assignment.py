02c9b0b8ef21f592b9364d81a277dc163812e7d7
dmartin28 <david_2004@DESKTOP-KIKK8A6>
2025-02-14 16:42:04 -0800

    none

--- phase_2/IP_assignment.py
+++ phase_2/IP_assignment.py
@@ -3,12 +3,11 @@ def IP_assignment(robot_list, task_list, L):
     n = len(robot_list)
     m = len(task_list)
 
-
-    # C
-    # assignment is a 2D ragged list First index corresponds to tasks.
+    # Assignment is a 2D ragged list first index corresponds to tasks,
+    # second is index of robot in robotlist.
     assignment = [[]for _ in range(m)]
-    for robot in robot_list:
-        assignment[robot.id % m].append(robot.id)
+    for i in range(0,n):
+        assignment[robot_list[i] % m].append(robot_idx)
     return assignment
 
 def IP_recursion(robot_list, task_list, task_max_vals L):
